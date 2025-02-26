d5e37c571f99b6686d7b3a9374eb6eb96b0b87da
Tom-Oliver Marek <tom-oliver@marek.nrw>
2025-02-26 19:10:32 +0100

    0.0.11 ConvertStringBooleanAttribute

---
+++ Convertification/Attributes/ConvertStringBooleanAttribute.cs
@@ -0,0 +1,37 @@
+﻿namespace Convertification.Attributes;
+
+public class ConvertStringBooleanAttribute : JsonConverterAttribute
+{
+    public ConvertStringBooleanAttribute(): base(typeof(StringBooleanConverter))
+    {
+        
+    }
+}
+
+public class StringBooleanConverter : JsonConverter<bool>
+{
+    public override bool Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        try
+        {
+            return reader.TokenType switch
+            {
+                JsonTokenType.String => reader.GetBoolean(),
+                JsonTokenType.False => false,
+                JsonTokenType.True => true,
+                JsonTokenType.Number => reader.GetDouble() != 0,
+                _ => throw new InvalidOperationException($"Unexpected JsonTokenType: {reader.TokenType}"),
+            };
+        }
+        catch (Exception ex)
+        {
+            throw new JsonException("", ex);
+        }
+    }
+
+    public override void Write(Utf8JsonWriter writer, bool value, JsonSerializerOptions options)
+    {
+        // Schreib den decimal-Wert als String in JSON
+        writer.WriteBooleanValue(value);
+    }
+}
